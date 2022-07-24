f7c2b4f5a259251660ea8619d016d8dbd2786782
Akiko Takano <akiko.pusu@gmail.com>
2022-07-24 18:03:11 +0900

    Initial import.

---
+++ src/main/java/com/example/myFirstDemo/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.myFirstDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
